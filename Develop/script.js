65951ae491895120f528afe2ee2e181ad87a6307
Noah Wyche <noahwyche1@gmail.com>
2023-10-03 11:17:09 -0400

    further fleshed out the code and prepared it for debugging an finalization

--- Develop/script.js
+++ Develop/script.js
@@ -2,22 +2,37 @@
 // changed from var to const for clarity and readability
 // Function to generate a random password based off of submitted critirea
 function generatePassword() {
-    const length = parseInt(promt("enter password length:"),10);
+    const length = parseInt(promt('enter password length:'), 10);
 
-    if(isNaN(length) || length <= 0) {
-        alert("Please enter a valid password length");
-        return "";
+    if (isNaN(length) || length <= 0) {
+        alert('Please enter a valid password length.');
+        return '';
     }
 
     // These are the critirea options shown to select for a new password
-    const lowercaseChars = "abcdefghjklmnopqrstuvwxyz"
-    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    const numericChars = "0123456789"
-    const specialChars = "!@#$%^&*()_+[]{}|;:,.<>?"
-}
+    const lowercaseChars = 'abcdefghjklmnopqrstuvwxyz';
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numericChars = '0123456789';
+    const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?';
 
-// Get references to the #generate element
-const generateBtn = document.querySelector("#generate");
+    let availableChars = lowercaseChars;
+    const includeUppercase = confirm('Include uppercase characters?');
+    const includeNumbers = confirm('Include numbers?');
+    const includeSpecial = confirm('Include special characters?')
+
+    if (includeUppercase) availableChars += uppercaseChars;
+    if (includeNumbers) availableChars += numericChars;
+    if (includeSpecial) availableChars += specialChars;
+
+    let password = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * availableChars.length);
+        password += availableChars[randomIndex];
+    }
+
+    return password;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -28,5 +43,7 @@ function writePassword() {
 
 }
 
+// Get references to the #generate element
+const generateBtn = document.querySelector('#generate');
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
