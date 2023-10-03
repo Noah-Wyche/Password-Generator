8e90832556ee467a10e6ae55ed6f1101c2c38743
Noah Wyche <noahwyche1@gmail.com>
2023-10-03 10:45:35 -0400

    changed var to const for readability and ease of use. I also began adding functionality to the password generator to ask for criterea for creating a password.

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,28 @@
 // Assignment code here
+// changed from var to const for clarity and readability
+// Function to generate a random password based off of submitted critirea
+function generatePassword() {
+    const length = parseInt(promt("enter password length:"),10);
 
+    if(isNaN(length) || length <= 0) {
+        alert("Please enter a valid password length");
+        return "";
+    }
+
+    // These are the critirea options shown to select for a new password
+    const lowercaseChars = "abcdefghjklmnopqrstuvwxyz"
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const numericChars = "0123456789"
+    const specialChars = "!@#$%^&*()_+[]{}|;:,.<>?"
+}
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
